fdfb2392c7f98214d538b0cc362182caee5782a2
Valerie <valeriealexarivera@gmail.com>
2025-08-08 09:52:07 -0400

    added friend class

---
+++ Friend .swift
@@ -0,0 +1,20 @@
+//
+//  Friend .swift
+//  Birthdays
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import Foundation
+
+//adding name and birthday
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+    
+}// end of Friend 
